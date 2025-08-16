dd1e6439daae117f20042ebd23b20c4c5adf954d
cloudhead777 <keerthan.rez@gmail.com>
2025-08-16 23:41:46 +0530

    tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,5 @@
 """
-Tic Tac Toe Player
+Tic Tac Toe Player with Minimax
 """
 
 import copy
@@ -20,7 +20,13 @@ def initial_state():
 
 
 def player(board):
-        return X
+    """
+    Returns player who has the next turn on a board.
+    """
+    X_count = sum(row.count(X) for row in board)
+    O_count = sum(row.count(O) for row in board)
+    return X if X_count <= O_count else O
+
 
 def actions(board):
     """
@@ -30,53 +36,58 @@ def actions(board):
     for row in range(len(board)):
         for col in range(len(board[0])):
             if board[row][col] == EMPTY:
-                action.add((row,col))
+                action.add((row, col))
+    return action
 
-    return action 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    first = player(board)
-    comp = deepcopy(board)
+    if action not in actions(board):
+        raise Exception("Invalid move")
+
+    comp = copy.deepcopy(board)
     i, j = action
-    if board[i][j] != None:
-        raise Exception
-    else:
-        comp[i][j] = first
+    comp[i][j] = player(board)
     return comp
+
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
+    # Check rows and columns
     for y in range(3):
         if (board[y][0] == board[y][1] == board[y][2]) and (board[y][0] != EMPTY):
             return board[y][0]
         if (board[0][y] == board[1][y] == board[2][y]) and (board[0][y] != EMPTY):
             return board[0][y]
-    if (board[0][0] == board[1][1] == board[2][2]) or (board[0][2] == board[1][1] == board[2][0]) \
-            and board[1][1] != EMPTY:
+
+    # Check diagonals
+    if ((board[0][0] == board[1][1] == board[2][2]) or
+        (board[0][2] == board[1][1] == board[2][0])) and board[1][1] != EMPTY:
         return board[1][1]
+
     return None
+
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    if winner(board) == X or winner(board) == O:  # check if there is a winner or not
+    if winner(board) is not None:
         return True
-    elif EMPTY not in board[0] and EMPTY not in board[1] and EMPTY not in board[2]:
+    elif all(EMPTY not in row for row in board):
         return True
-    else:
-        return False
+    return False
+
+
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
     win_player = winner(board)
-
     if win_player == X:
         return 1
     elif win_player == O:
@@ -91,40 +102,35 @@ def minimax(board):
     """
 
     def maxvalue(board):
-        bestmove = ()
+        bestmove = None
         if terminal(board):
             return utility(board), bestmove
-        else:
-            v = -5
-            for action in actions(board):
-                minval = minvalue(result(board, action))[0]
-                if minval > v:
-                    v = minval
-                    bestmove = action
-            return v, bestmove
 
+        v = -math.inf
+        for action in actions(board):
+            minval = minvalue(result(board, action))[0]
+            if minval > v:
+                v = minval
+                bestmove = action
+        return v, bestmove
 
     def minvalue(board):
-        bestmove = ()
+        bestmove = None
         if terminal(board):
             return utility(board), bestmove
-        else:
-            v = 5
-            for action in actions(board):
-                maxval = maxvalue(result(board, action))[0]
-                if maxval < v:
-                    v = maxval
-                    bestmove = action
-            return v, bestmove
 
-
-    currentplayer = player(board)
+        v = math.inf
+        for action in actions(board):
+            maxval = maxvalue(result(board, action))[0]
+            if maxval < v:
+                v = maxval
+                bestmove = action
+        return v, bestmove
 
     if terminal(board):
         return None
 
-    if currentplayer == X:
+    if player(board) == X:
         return maxvalue(board)[1]
-
     else:
         return minvalue(board)[1]
